0bf069389df4ffd0fcf0318e1285741262591abc
harsha-helix <harshavardhan.2003.nov@gmail.com>
2026-05-07 11:27:40 +0530

    fixing whitescreen

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Add this line below, and replace with your EXACT repository name
+  base: '/portfolio/', 
 })
